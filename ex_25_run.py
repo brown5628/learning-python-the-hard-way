9e88030eaebe8cf3e77c12046246d08d7de6dd4f
Will Brown <brown5628@gmail.com>
2019-11-04 07:42:29 -0500

    Ex 24 -31

---
+++ ex_25_run.py
@@ -0,0 +1,15 @@
+import ex_25
+sentence = "All good things come to those who wait."
+words = ex_25.break_words(sentence)
+words
+sorted_words = ex_25.sort_words(words)
+sorted_words
+ex_25.print_first_word(words)
+ex_25.print_last_word(words)
+words
+ex_25.print_first_word(sorted_words)
+ex_25.print_last_word(sorted_words)
+sorted_words
+sorted_words = ex_25.sort_sentence(sentence)
+sorted_words
+ex_25.print_first_and_last(sentence)
